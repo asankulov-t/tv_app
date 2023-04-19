29957887142c3b06f6063434da9558e6a6cfdc75
unknown <asankulov-t>
2023-04-19 21:20:56 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App = () => {
+    let [movies,setMovies]=useState();
+    const url =
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=f5a9497cc4b806e5898ca24b00f5cf33&language=ru&page=3";
+    useEffect(() => {
+        fetchPopular();
+    }, []);
+    const fetchPopular = async () => {
+        const data = await fetch(url);
+        const res = await data.json();
+        setMovies(res)
+        console.log(res);
+    };
+
+
+
+    const AutoplaySlider = withAutoplay(AwesomeSlider);
+
+    const slider = (
+        <AutoplaySlider
+            infinite={true}
+            buttons={false}
+            play={true}
+            cancelOnInteraction={false} // should stop playing on user interaction
+            interval={5000}
+            bullets={false}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+            {movies&&movies.results.map((item)=>{
+                return <img data-src={"https://image.tmdb.org/t/p/w500"+item.poster_path}/>
+            })}
+        </AutoplaySlider>
+    );
 
+    //"https://image.tmdb.org/t/p/w500"
+    return (
+        <div className="App">
+            {slider}
+        </div>
+    );
+};
 export default App;
---
+++ src/Slide.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Slider from "react-slick";
+
+const Slide = (props) => {
+    console.log(props.props)
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Slide;
